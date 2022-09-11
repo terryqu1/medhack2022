87761d000f3d5d8bdfb21cd4953737c9377c87ad
[Terry] <email>
2022-09-11 02:07:17 -0400

    created an about page and router for it

--- embedding/app.py
+++ embedding/app.py
@@ -9,6 +9,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 
 @app.route('/result',methods=['POST', 'GET'])
